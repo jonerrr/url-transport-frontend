f0613a937f702892477ca28ef5fe038795e424f4
jonah <73760377+jonerrr@users.noreply.github.com>
2022-01-18 19:38:07 -0500

    fix unused imports

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,26 +1,21 @@
-import { ReactNode } from "react"
-import Head from "next/head"
-import { Container, Flex, Heading, HStack } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { NextChakraLink } from "./NextChakraLink"
+import { ReactNode } from "react";
+import Head from "next/head";
+import { Container } from "@chakra-ui/react";
+// import { ColorModeSwitcher } from "./ColorModeSwitcher"
+// import { NextChakraLink } from "./NextChakraLink"
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-export const Layout = ({
-  children,
-  title,
-}: Props) => (
+export const Layout = ({ children, title }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <Container maxWidth="1200px">
-      {children}
-    </Container>
+    <Container maxWidth="1200px">{children}</Container>
   </div>
-)
+);
